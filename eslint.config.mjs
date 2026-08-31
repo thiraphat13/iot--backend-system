e85dbf43b422ddff7ef9a2c253bfc12d49f1e0c0
thiraphat13 <thiraphat.khu@ku.th>
2026-08-31 11:39:29 +0700

    check

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  eslintConfigPrettier, // ใส่เพิ่มไว้ด้านท้ายสุด
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
